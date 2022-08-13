4c80bd01ca8ffeb2bc5fdf00b620c07c1a77ea58
neweos <65535758+neweos@users.noreply.github.com>
2022-08-13 22:34:54 +0800

    neweos created a commit :sparkles:

---
+++ src/utils/filepath.ts
@@ -0,0 +1,17 @@
+function getFile(path: string) {
+  return path
+    .replace(/^pages/, '.')
+    .replace(/[\[\]]/g, '_')
+}
+
+function getPattern(path: string) {
+  return path
+    .replace(/\.js$/, '')
+    .replace(/^pages/, '')
+    .replace(/index$/, '')
+    .replace(/\[([_a-z]+)\]/g, ':$1')
+}
+
+function getComponent(path: string) {
+  return path.replace(/[\[\]\-\/\.]/g, '_').toUpperCase()
+}
