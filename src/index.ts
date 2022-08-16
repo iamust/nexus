c7aa94b27836e955f55944f0fc035744004b3bb2
neweos <65535758+neweos@users.noreply.github.com>
2022-08-16 23:09:56 +0800

    neweos created a commit :sparkles:

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,53 @@
+import { copyPage, outputAppComponent, outputNotFoundComponent } from './utils'
+import { outputNexusJS } from './helpers/nexus'
+import { outputHtml } from './helpers/html'
+import { pathExistsSync } from 'fs-extra'
+import { getPaths, page } from './page'
+import chokidar from 'chokidar'
 
+function addCallback(path: string) {
+  copyPage(path)
+  page.addRoute(path)
+  outputNexusJS(page.routes())
+}
 
+function changeCallback(path: string) {
+  copyPage(path)
+}
 
+function unlinkCallback(path: string) {
+  page.removeRoute(path)
+  outputNexusJS(page.routes())
+}
 
+export function watch() {
+  chokidar
+    .watch('pages/**/*.{tsx,jsx}')
+    .on('add', addCallback)
+    .on('change', changeCallback)
+    .on('unlink', unlinkCallback)
+}
 
-// import chokidar from 'chokidar'
-// export class Bundler {
-// const bundler = new Bundler(options)
-// import fs from 'fs-extra'
-// import { pathExistsSync } from 'fs-extra'
-//   watch() {
-//     chokidar
-//       .watch('pages/**/*.{tsx,jsx}')
-//       .on('add', (path) => {
-//         copyNexusPage(path)
-//         this.addRoute(path)
-//         // outputNexusJS(this.routes())
-//       })
-//       .on('change', (path) => {
-//         copyNexusPage(path)
-//       })
-//       .on('unlink', (path) => {
-//         this.removeRoute(path)
-//         // outputNexusJS(this.routes())
-//       })
-//   }
+export async function setup() {
+  const paths = getPaths()
 
-// const paths = await getPaths()
-// await trash('.nexus/**/*')
-// for (let path of paths) {
-//   copyNexusPage(path)
-// }
-// addPaths|paths
-// outputNexusJS
-// copyNexusPage
-// getPaths
-// await glob('pages/**/*.js')
-// getEntryPath
+  for (const path of paths) {
+    copyPage(path)
+  }
 
-// const config = buildOptions(opts)
-// const bundler = new Bundler()
-// bundler.serve()
-// page.watch()
+  if (!pathExistsSync('index.html')) {
+    outputNexusJS(page.routes())
+  }
 
-// src/index.ts
-// src/tasks/build.ts
-// src/tasks/serve.ts
+  if (!pathExistsSync('.nexus/main.ts')) {
+    outputNexusJS(page.routes())
+  }
+
+  if (!pathExistsSync('.nexus/app.tsx')) {
+    outputAppComponent()
+  }
+
+  if (!pathExistsSync('.nexus/404.tsx')) {
+    outputNotFoundComponent()
+  }
+}
