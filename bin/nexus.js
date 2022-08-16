f984bdacef3a642aebfa16818a4358832e4ed62e
neweos <65535758+neweos@users.noreply.github.com>
2022-08-16 11:34:36 +0800

    neweos created a commit :sparkles:

--- bin/nexus.js
+++ bin/nexus.js
@@ -1,22 +1,3 @@
 #!/usr/bin/env node
 
 // compiler.run((err, stats) => {
-//   if (err) {
-//     console.error(err.stack || err)
-//     if (err.details) {
-//       console.error(err.details)
-//     }
-//     return
-//   }
-//   const info = stats.toJson()
-//   if (stats.hasErrors()) {
-//     console.error(info.errors)
-//   }
-//   if (stats.hasWarnings()) {
-//     console.warn(info.warnings)
-//   }
-//   console.log(stats.toString())
-//   compiler.close((closeErr) => {
-//     console.log(closeErr)
-//   })
-// })
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,5 @@
 import type { Configuration } from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import { WebpackManifestPlugin } from 'webpack-manifest-plugin'
-import rules from './rules'
-
-type NexusConfig = Configuration
 
 const root = process.cwd()
 const mode = process.env.NODE_ENV || 'development'
@@ -18,39 +14,4 @@ export interface UserConfig {
 
 export function defineConfig(config: UserConfig) {
   const isProd = mode === 'production'
-  const outDir = config.outDir || 'out'
-  const serverPort = 3323
-  const serverHmr = !isProd
-  const plugins = [
-    new WebpackManifestPlugin({}),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
-  ]
-  const nexusConfig: NexusConfig = {
-    entry: {
-      nexus: './index.js'
-    },
-    output: {
-      path: `${root}/${outDir}`,
-      filename: isProd ? '[name].[contenthash].js' : '[name].js'
-    },
-    devtool: false,
-    devServer: {
-      hot: serverHmr,
-      port: serverPort
-    },
-    module: {
-      rules: rules({ isProd })
-    },
-    plugins,
-  }
 }
-
-// build: {
-//   manifest: boolean
-//   minify: boolean
-//   assetsDir: string
-// }
-// defaultLoaders
-// defaultConfig
